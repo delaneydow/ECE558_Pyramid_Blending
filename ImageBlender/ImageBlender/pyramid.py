99788e4550154474031718f6eb1a18eaf7fcaea4
Tyler Williams <tylerdwilliams@me.com>
2025-11-20 15:56:06 -0500

    down and up sampling

--- ImageBlender/ImageBlender/pyramid.py
+++ ImageBlender/ImageBlender/pyramid.py
@@ -1,4 +1,20 @@
+import numpy as np
 
+def downsample(image):
+    return image[::2, ::2, ...]
+
+def upsample(image, target_shape):
+    up = np.repeat(np.repeat(image, 2, axis=0), 2, axis=1)
+    th, tw = target_shape[0], target_shape[1]
+
+    if up.ndim == 3:
+        up_cropped = up[:th, :tw, ...] 
+    else:
+        up_cropped = up[:th, :tw]
+    return up_cropped
 
 def ComputePyr(input_image, num_layers):
-    return 0
+    gPyr = []
+    lPyr = []
+    
+    return gPyr, lPyr
